beab3a1ff256c14787e81365b9db9a92186cd5f5
Zach H <zdhadgraft@gtcc.edu>
2016-06-01 16:12:32 -0400

    Numbers can be POSTED, GETTED, and bad values are rejected.

--- app.rb
+++ app.rb
@@ -5,7 +5,91 @@ require "pry"
 DB = {}
 
 class Coloson < Sinatra::Base
-  # Your code goes here ...
+  def self.reset_database
+    DB.clear
+  end
+
+  def t_string
+   try_to_i(params["number"])
+  end
+
+  def try_to_i string
+   if string == string.to_i.to_s
+     return string.to_i
+   end
+   string
+  end
+
+  def is_valid? string
+    string == string.to_i.to_s
+  end
+
+  get "/numbers/evens" do
+    if DB.empty?
+      status 200
+      json []
+    else
+      status 200
+      json DB["evens"]
+    end
+  end
+
+  get "/numbers/odds" do
+    if DB.empty?
+      status 200
+      json []
+    else
+      status 200
+      json DB["odds"]
+    end
+  end
+
+  post "/numbers/evens" do
+
+if is_valid? params["number"]
+    if DB["evens"]
+      DB["evens"].push t_string
+    else
+      DB["evens"] = [t_string]
+    end
+    status 200
+    json "success"
+
+    else
+
+      status 422
+      json "status": "error", "error": "Invalid number: #{params["number"]}"
+  end
+
+  end
+
+  post "/numbers/odds" do
+    if is_valid? params["number"]
+      if DB["odds"]
+        DB["odds"].push t_string
+      else
+        DB["odds"] = [t_string]
+      end
+      status 200
+      json "success"
+
+    else
+
+      status 422
+      json "status": "error", "error": "Invalid number: #{params["number"]}"
+
+    end
+  end
+
+  delete "/numbers/odds" do
+    status 200
+    binding.pry
+    DB["odds"].delete(t_string)
+
+    json "success"
+  end
+
+
 end
 
 Coloson.run! if $PROGRAM_NAME == __FILE__
--- tests.rb
+++ tests.rb
@@ -1,7 +1,7 @@
 require 'pry'
+require 'json'
 require 'minitest/autorun'
 require 'minitest/focus'
-
 require 'minitest/reporters'
 Minitest::Reporters.use! Minitest::Reporters::ProgressReporter.new
 
@@ -33,7 +33,12 @@ class ColosonTest < Minitest::Test
     response = get "/numbers/evens"
     assert_equal 200, response.status
     assert_equal [2,14,8], JSON.parse(response.body)
+    binding.pry
   end
+
+
+
+
 
   def test_it_resets_between_tests
     response = get "/numbers/evens"
@@ -47,21 +52,26 @@ class ColosonTest < Minitest::Test
     post "/numbers/odds", number: 13
 
     response = get "/numbers/odds"
+
     assert_equal [5,13], JSON.parse(response.body)
 
     delete "/numbers/odds", number: 5
+
     response = get "/numbers/odds"
     assert_equal [13], JSON.parse(response.body)
+
   end
+
+focus
 
   def test_it_wont_add_non_numbers
     response = post "/numbers/odds", number: "eleventy"
     assert_equal 422, response.status
-
     body = JSON.parse response.body
     assert_equal "error", body["status"]
     assert_equal "Invalid number: eleventy", body["error"]
   end
+
 
   def test_it_can_sum_numbers
     post "/numbers/primes", number: 7
